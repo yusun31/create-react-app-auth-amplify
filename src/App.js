9033172d290be932c9a2ac55402a62aef185f1d1
yusun <yousun2286@naver.com>
2022-03-17 15:10:28 +0900

    App.js edit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js for yusun</code> and save to reload.
+            Edit src/App.js for yusun and save to reload.
           </p>
           <a
             className="App-link"
